267b9d3aa2bdaae06a4840ef31a1c6fd4b636d5e
unknown <arthur12souza@hotmail.com>
2021-03-17 22:32:32 -0300

    inicio da tela de login com o placeholder dinamico pre configurado

--- src/view/Login/index.js
+++ src/view/Login/index.js
@@ -5,6 +5,33 @@ import './styles.css';
 export default function Login(){
 
     return(
-      <Header type="login"/>
+      <div className="container">
+        <div className="logo">
+          <nobr style={{color: '#E5E5E5'}}>Acompanha</nobr>
+          <nobr style={{color: '#EC625F'}}>Série</nobr>
+        </div>
+        
+        <div className="box">
+
+          <div className="left-box"></div>
+          <div className="right-box">
+            <h1>Entrar</h1>
+            <form className="formulario">
+              <div>
+                <input type="text" name="" required className="entrada entrarCampos"/>
+                <label>Email:</label>
+              </div>
+              {/* <label>
+                email: 
+                <input type="text" className="entrada entrarCampos"/>
+              </label>
+              <label>
+                senha: 
+                <input type="text" className="entrada entrarCampos"/>
+              </label> */}
+            </form>
+          </div>
+        </div>
+      </div>
     );  
 }
